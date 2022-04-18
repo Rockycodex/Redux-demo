2edf81efc1a0fd7936d2856939ebcc1bd785fcfc
Nidhi Verma <vnidhi235@gmail.com>
2022-04-18 15:11:01 +0530

    Create new todo feature

--- src/components/CreateNewToDo.js
+++ src/components/CreateNewToDo.js
@@ -1,3 +1,35 @@
+import React, {useState} from 'react';
+import Box from '@mui/material/Box';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import TO_DO_LIST from '../constants/todos';
+
+
 export default function CreateNewToDo() {
-    return <h2>Create New To-Do</h2>;
+    const [todoText, setTodoText] = useState('');
+
+    const handleNewToDo = () => {
+        TO_DO_LIST.push({
+            id: TO_DO_LIST.length+1,
+            text: todoText
+        });
+        setTodoText('');
+    }
+
+    const handleToDoTextChange = (event) => {
+        const text = event.target.value;
+        setTodoText(text);
+    }
+
+    return (
+        <>
+            <Box>
+                <h3>Create new task</h3>
+                <TextField onChange={handleToDoTextChange} value={todoText} id="outlined-basic" label="Enter the task.." variant="outlined" />
+                <br />
+                <br />
+                <Button onClick={handleNewToDo} variant="contained">Save</Button>
+            </Box>
+        </>
+    );
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ToDo from "./ToDo";
 import TO_DO_LIST from "../constants/todos";
+import Box from '@mui/material/Box';
 
 const ToDoList = () => {
     return (
         <>
-            {TO_DO_LIST.map(todo => <ToDo key={todo.id} data={todo}></ToDo>)}
+        <Box>
+            {TO_DO_LIST.map(todo => 
+            <ToDo  key={todo.id} data={todo}></ToDo>)}
+            </Box>
         </>
     )
 }
